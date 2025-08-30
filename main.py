7a9097203b2c2f5c3fee2024152b6ff7db9d5b86
ruchit2005 <ruchitgupta05@gmail.com>
2025-08-30 10:34:27 +0530

    First commit with initialization of OpenAI

---
+++ main.py
@@ -0,0 +1,15 @@
+from openai import OpenAI
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+api_key = os.getenv("OPENAI_API_KEY")
+client = OpenAI(api_key=api_key)
+
+response = client.responses.create(
+    model="gpt-5-mini",
+    input="Write a one-sentence bedtime story about a unicorn."
+)
+
+print(response.output_text)
